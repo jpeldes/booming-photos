3308ccc4bcf54729c3013be83729a079967d83f4
Jaak Peldes <jaakpeldes@gmail.com>
2023-09-19 17:14:13 +0300

    Set up Prettier

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders hello world', () => {
-  render(<App />);
-  const helloWorldText = screen.getByText(/hello world/i);
-  expect(helloWorldText).toBeInTheDocument();
-});
+    render(<App />)
+    const helloWorldText = screen.getByText(/hello world/i)
+    expect(helloWorldText).toBeInTheDocument()
+})
